f412d49827daa98d8c6632b3107a1dcbb7c31da0
Guoqi <maguoqi96@gmail.com>
2020-02-14 13:04:13 -0500

    add player ShotChart

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,21 @@ import React, {Component} from 'react';
 
 import nba from '../nba-client';
 import Profile from './Profile';
+import ShotChart from './ShotChart';
 
 class Main extends Component {
     state = {
-        playerInfo: {}
+        playerInfo: {},
+        playerId: 201939
     }
 
     componentDidMount() {
         window.nba = nba;
         nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
             .then((info) => {
-                console.log(info);
+                //console.log(info);
                 const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-                console.log(playInfo);
+                //console.log(playInfo);
                 this.setState({ playerInfo: playInfo });
             })
     }
@@ -23,8 +25,10 @@ class Main extends Component {
         return (
             <div className="main">
                 <Profile playerInfo={this.state.playerInfo} />
+                <ShotChart playerId={this.state.playerId}/>
             </div>
         );
+
     }
 }
 
