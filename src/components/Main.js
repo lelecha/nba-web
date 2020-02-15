a6e08fe9fc328821255d924cde4aa9b4dd121675
Guoqi <maguoqi96@gmail.com>
2020-02-15 14:21:03 -0500

    finish search

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,46 @@
+
 import React, {Component} from 'react';
 
 import nba from '../nba-client';
 import Profile from './Profile';
-import SearchBar from "./SearchBar";
-
 import DataViewContainer from './DataViewContainer';
+import SearchBar from './SearchBar';
+
+import { DEFAULT_PLAYER_INFO } from '../constants';
+
 class Main extends Component {
     state = {
-        playerInfo: {},
-        playerId: 201939
+        playerInfo: DEFAULT_PLAYER_INFO
     }
 
     componentDidMount() {
         window.nba = nba;
-        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
-            .then((info) => {
-                //console.log(info);
-                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
-                //console.log(playInfo);
-                this.setState({ playerInfo: playInfo });
-            })
+        this.loadPlayerInfo('Stephen Curry');
+    }
+
+    loadPlayerInfo = (playerName) => {
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId}).then((info) => {
+            console.log(info);
+            const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playInfo);
+            this.setState({ playerInfo: playInfo });
+        })
+    }
+
+    handleSelectPlayer = (playerName) => {
+        this.loadPlayerInfo(playerName);
     }
 
     render() {
         return (
             <div className="main">
-                <SearchBar />
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
                 <div className="player">
                     <Profile playerInfo={this.state.playerInfo} />
-                    <DataViewContainer playerId={this.state.playerId}/>
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
                 </div>
             </div>
-
-
         );
-
     }
 }
 
